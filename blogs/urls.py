dbdf7cdbeba02263342863d63dbf92d7bc3cd3f8
Nick Kitchel <nkitchel@frontier.com>
2020-10-15 15:01:55 -0600

    Added a link to create a new blog post. Edited the homepage, also did some work with the formatting of the blog page.

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView, BlogListView
+from .views import HomePageView, BlogListView, BlogDetailView, BlogCreateView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('blog/post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('blog/post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('blog/', BlogListView.as_view(), name='blog'),
     ]
