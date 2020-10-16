6ab721d5deffe306abd6a43994edd758d19f0f9e
Nick Kitchel <nkitchel@frontier.com>
2020-10-16 09:48:06 -0600

    Added features to edit and delete blog postings, this is done when clicking on a blog. Also ran the tests file from moodle and managed to get it working

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from .views import HomePageView, BlogListView, BlogDetailView, BlogCreateView
+from .views import (
+    HomePageView, 
+    BlogListView, 
+    BlogDetailView, 
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+    )
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('blog/post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('blog/post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('blog/post/new/', BlogCreateView.as_view(), name='post_new'),
     path('blog/post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('blog/', BlogListView.as_view(), name='blog'),
